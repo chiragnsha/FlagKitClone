8fca136a292253393cbd212998e9d3df4973ded2
Chirag <chirag.shah@zoho>
2019-08-12 13:20:54 +0530

    removed source restructed for test

--- Sources/Swift/build/FlagKit.build/Debug-iphonesimulator/FlagKit.build/Objects-normal/i386/FlagKit_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/chirag-3369/Downloads/FlagKit-master/components/thirdparty_packages/flagkit/v2_0/flagkit_master/Sources/Swift/build/FlagKit.build/Debug-iphonesimulator/FlagKit.build/DerivedSources/FlagKit_vers.c
