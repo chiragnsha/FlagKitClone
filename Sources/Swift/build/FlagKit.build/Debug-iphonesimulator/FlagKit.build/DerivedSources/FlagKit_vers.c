3a82070f3256ded10654e156bede69e2c34f18db
Chirag <chirag.shah@zoho>
2019-08-12 12:28:28 +0530

    Cloned source added

---
+++ Sources/Swift/build/FlagKit.build/Debug-iphonesimulator/FlagKit.build/DerivedSources/FlagKit_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char FlagKitVersionString[];
+ extern const double FlagKitVersionNumber;
+
+ const unsigned char FlagKitVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:FlagKit  PROJECT:FlagKit-1" "\n";
+ const double FlagKitVersionNumber __attribute__ ((used)) = (double)1.;
